77b2633b5d67fa9bf54e9dd156d2aec0814a2b36
Martin Hildebrandt <der.antikeks@googlemail.com>
2013-05-07 12:26:38 +0200

    single ops tests

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Schemaless SQL
 
 __TODO:__
+* support unexported values
 * query functions (Find, FindOne)
 * lots of tests
 * documentation/cleanup
--- schemalessql_test.go
+++ schemalessql_test.go
@@ -1,7 +1,9 @@
 package schemalessql_test
 
 import (
+	"database/sql"
 	"github.com/der-antikeks/schemalessql"
+	"reflect"
 	"testing"
 	"time"
 
@@ -39,19 +41,80 @@ func TestCreate(t *testing.T) {
 	db := newDB(t)
 	defer closeDB(t, db)
 
-	e := Entity{123, 123.456, true, []byte{12, 34, 56}, "foo", time.Now(), time.Duration(3)}
-	_, err := db.Put(nil, e)
-
-	if err != nil {
+	e := Entity{123, 123.456, true, []byte{12, 34, 56}, "foo", time.Now(), time.Duration(3) * time.Minute}
+	if _, err := db.Put(nil, e); err != nil {
 		t.Fatalf("error creating entity: %v", err)
 	}
 }
 
-// TODO
+func TestRead(t *testing.T) {
+	db := newDB(t)
+	defer closeDB(t, db)
 
-func TestRead(t *testing.T)   {}
-func TestUpdate(t *testing.T) {}
-func TestDelete(t *testing.T) {}
+	e := Entity{123, 123.456, true, []byte{12, 34, 56}, "foo", time.Now(), time.Duration(3) * time.Minute}
+	key, err := db.Put(nil, e)
+	if err != nil {
+		t.Fatalf("error creating entity: %v", err)
+	}
+
+	var r Entity
+	if err := db.Get(key, &r); err != nil {
+		t.Fatalf("error reading entity: %v", err)
+	}
+
+	if !reflect.DeepEqual(e, r) {
+		t.Fatalf("entities do not match: \n%v\n%v", e, r)
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	db := newDB(t)
+	defer closeDB(t, db)
+
+	e := Entity{123, 123.456, true, []byte{12, 34, 56}, "foo", time.Now(), time.Duration(3) * time.Minute}
+	key, err := db.Put(nil, e)
+	if err != nil {
+		t.Fatalf("error creating entity: %v", err)
+	}
+
+	u := e
+	u.E = "updated data"
+	u.F = time.Now().Add(e.IgnoreMe.(time.Duration))
+	if _, err := db.Put(key, u); err != nil {
+		t.Fatalf("error updating entity: %v", err)
+	}
+
+	var r Entity
+	if err := db.Get(key, &r); err != nil {
+		t.Fatalf("error reading entity: %v", err)
+	}
+
+	if reflect.DeepEqual(e, u) || !reflect.DeepEqual(u, r) {
+		t.Fatalf("entities do not match: \n%v\n%v", u, r)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	db := newDB(t)
+	defer closeDB(t, db)
+
+	e := Entity{123, 123.456, true, []byte{12, 34, 56}, "foo", time.Now(), time.Duration(3) * time.Minute}
+	key, err := db.Put(nil, e)
+	if err != nil {
+		t.Fatalf("error creating entity: %v", err)
+	}
+
+	if err := db.Delete(key); err != nil {
+		t.Fatalf("error deleting entity: %v", err)
+	}
+
+	var r Entity
+	if err := db.Get(key, &r); err != sql.ErrNoRows {
+		t.Fatalf("failed to delete entity: %v", err)
+	}
+}
+
+// TODO
 
 func TestCreateMulti(t *testing.T) {}
 func TestReadMulti(t *testing.T)   {}
